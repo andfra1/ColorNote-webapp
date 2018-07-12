1857d75cb7c7fc46542ba90a93ca722a0894bdc5
Franciszek Andruszkiewicz <fandruszkiewicz@gmail.com>
2018-07-12 22:58:29 +0200

    note chcecking now is more usable

--- includes/notes.php
+++ includes/notes.php
@@ -1,16 +1,23 @@
 <div class="notes">
   <?php for($n=0; $n<2; $n++) : ?>
-    <input type="checkbox" name="note-<?= $n; ?>" id="note-<?= $n; ?>">
-    <label class="notes__item" for="note-<?= $n; ?>">
-      <div class="notes__item__title">Item</div>
-      <div class="notes__item__checklist"></div>
-      <div class="notes__item__date">13 sty</div>
+
+  <input type="checkbox" name="note-<?= $n; ?>" id="note-<?= $n; ?>">
+  <div class="notes__item">
+    <div class="notes__item__title">Item</div>
+    <div class="notes__item__checklist"></div>
+    <div class="notes__item__date">13 sty</div>
+    <label class="notes__item__check" for="note-<?= $n; ?>">
     </label>
-    <input type="checkbox" name="note-0<?= $n; ?>" id="note-0<?= $n; ?>">
-    <label class="notes__item" for="note-0<?= $n; ?>">
-      <div class="notes__item__title">Item</div>
-      <div class="notes__item__checklist notes__item__checklist--isChecklist"></div>
-      <div class="notes__item__date">13 sty</div>
-    </label>
-  <?php endfor; ?>
   </div>
+  
+  <input type="checkbox" name="note-0<?= $n; ?>" id="note-0<?= $n; ?>">
+  <div class="notes__item">
+    <div class="notes__item__title">Item</div>
+    <div class="notes__item__status notes__item__status--checklist"></div>
+    <div class="notes__item__date">13 sty</div>
+
+    <label class="notes__item__check" for="note-0<?= $n; ?>">
+    </label>
+  </div>
+  <?php endfor; ?>
+</div>
