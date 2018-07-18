95a9b3374dd7ceae052b6d53b73385e529f3399c
Franciszek Andruszkiewicz <fandruszkiewicz@gmail.com>
2018-07-18 21:31:55 +0200

    delete note

---
+++ _dev/js/_delete.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+var del = document.getElementById('jsDelete');
+var note = document.getElementsByClassName('notes__item');
+
+del.addEventListener('click', deleteNote, false);
+
+function deleteNote() {
+  for (var n = 0; n < note.length; n++) {
+    if (note[n].firstChild.checked) {
+      note[n].style.display = "none";
+    }
+  }
+}
--- prod/js/main.js
+++ prod/js/main.js
@@ -50,6 +50,20 @@ function setColor() {
     }
   }
 }
+/*jshint esversion: 6 */
+
+var del = document.getElementById('jsDelete');
+var note = document.getElementsByClassName('notes__item');
+
+del.addEventListener('click', deleteNote, false);
+
+function deleteNote() {
+  for (var n = 0; n < note.length; n++) {
+    if (note[n].firstChild.checked) {
+      note[n].style.display = "none";
+    }
+  }
+}
 if ($('#jsonHere')) {
   $.getJSON("./json/notes.json", function (data) {
     var items = [];
